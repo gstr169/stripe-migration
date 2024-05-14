b41acf1f7900e4a3c07847a72f792ef078bd0924
dmitry.finko <dmit.finn@yandex.ru>
2024-05-14 22:48:42 +0300

    Updated copy_subscriptions.py

--- copy_subscriptions.py
+++ copy_subscriptions.py
@@ -5,13 +5,53 @@ import datetime
 
 import stripe
 
+# Settings for script
+
 source = "YOUR_SOURCE_ACCOUNT_STRIPE_KEY"
 dest = "YOUR_DESTINATION_ACCOUNT_STRIPE_KEY"
+
+PRICES_IDS = {
+    # "{OLD_PRICE_ID}": "{NEW_PRICE_ID}"
+}
 
 
 def update_db(dest_customer, dest_subscription):
     # if you need to update your database, you'll need to
     # add a database update method here. In my case, I'm using MongoDB
+
+    # db_customer = db.users.find_one({"email": dest_customer.email})
+    # if not db_customer:
+    #     print(
+    #         "Something went wrong. Customer", dest_customer.email, "not found in db"
+    #     )
+    #
+    # if not dest_customer.email:
+    #     print("Customer does not have email, will try to update by stripeId")
+    #     db.users.update(
+    #         {"stripeId": dest_customer.id},
+    #         {
+    #             "$set": {
+    #                 "stripeId": dest_customer.id,
+    #                 "subscriptionId": dest_subscription.id,
+    #                 "hasSubscription": True,
+    #             }
+    #         },
+    #         multi=True,
+    #     )
+    # else:
+    #     db.users.update(
+    #         {"email": dest_customer.email},
+    #         {
+    #             "$set": {
+    #                 "stripeId": dest_customer.id,
+    #                 "subscriptionId": dest_subscription.id,
+    #                 "hasSubscription": True,
+    #             }
+    #         },
+    #         multi=True,
+    #     )
+    # print(dest_customer.email, "updated in database")
+
     pass
 
 
@@ -27,38 +67,22 @@ def time_str(timestamp):
 old_customers = stripe.Customer.list(limit=100, api_key=source)
 
 while old_customers:
-    for customer in old_customers.data:
+    for src_customer in old_customers.data:
         print("------------------------")
         print()
 
-        if not customer.subscriptions.data:
-            # if no subscription, just skip this customer
+        if not src_customer.subscriptions.data:
+            # if no subscriptions, just skip this customer
             print(
                 "No subscriptions found for:",
-                customer.email,
-                customer.id,
+                src_customer.email,
+                src_customer.id,
                 "- skipping.",
             )
             continue
+        print("Customer on source account is:", src_customer.email, src_customer.id)
 
-        # Get the subscription details
-        subscription = customer.subscriptions.data[0]
-
-        price = subscription.plan.id
-        plan_end_date = subscription.current_period_end
-
-        if subscription.cancel_at_period_end:
-            print("skip customer:", customer.email, "already cancelled.")
-            continue
-
-        print()
-        print("Customer on source account is:", customer.email, customer.id)
-        print("Plan on source account:", plan)
-        print("Plan on source account ends on:", time_str(plan_end_date))
-        print("Original subscription id:", subscription.id)
-        print()
-
-        dest_customer = stripe.Customer.retrieve(customer.id, api_key=dest)
+        dest_customer = stripe.Customer.retrieve(src_customer.id, api_key=dest)
 
         # if the customer already has a subscription
         # don't need to do anything
@@ -70,87 +94,66 @@ while old_customers:
             )
             continue
 
-        # your unique plan names here
-        new_price = ""
-        if plan == "standard-monthly":
-            new_price = "pro-monthly"
-        elif plan == "standard-annual":
-            new_price = "pro-annual"
-        elif plan == "basic-monthly":
-            new_price = "legacy-monthly"
-        elif plan == "basic-annual":
-            new_price = "legacy-annual"
+        # Get the subscription details
+        subscriptions = src_customer.subscriptions.data
+        for subscription in subscriptions:
+            price = subscription.price.id
+            subscription_end_date = subscription.current_period_end
 
-        if not new_price:
-            print("No corresponding plan found for:", plan)
-            continue
+            if subscription.cancel_at_period_end:
+                print("skip customer:", src_customer.email, "already cancelled.")
+                continue
 
-        if not plan_end_date:
-            print("Plan end date not found for:", customer.id)
-            continue
+            print()
+            print("Product on source account:", price.product)
+            print("Plan on source account ends on:", time_str(subscription_end_date))
+            print("Original subscription id:", subscription.id)
+            print()
 
-        print()
-        print("Now copying to dest customer:", dest_customer.email, dest_customer.id)
-        print("New plan is:", new_price)
-        print("New plan trial period ends:", time_str(plan_end_date))
-        print()
+            # your unique plan names here
+            new_price = PRICES_IDS.get(price.id)
 
-        # create the subscription for the new customer
-        dest_subscription = stripe.Subscription.create(
-            customer=dest_customer.id,
-            items=[{"price": new_price}],
-            trial_end=plan_end_date,
-            api_key=dest,
-        )
+            if not new_price:
+                print("No corresponding price found for:", price)
+                continue
 
-        print()
-        print("New subscription created:", dest_subscription.id)
-        print("New subscription type:", dest_subscription.plan.nickname)
-        print("New subscription start:", time_str(dest_subscription.start))
-        print()
+            if not subscription_end_date:
+                print("Plan end date not found for:", src_customer.id)
+                continue
 
-        # need to update the subscription id in our database
-        db_customer = db.users.find_one({"email": dest_customer.email})
-        if not db_customer:
+            print()
             print(
-                "Something went wrong. Customer", dest_customer.email, "not found in db"
+                "Now copying to dest customer:", dest_customer.email, dest_customer.id
+            )
+            print("New price is:", new_price)
+            print(
+                "New subscription trial period ends:", time_str(subscription_end_date)
+            )
+            print()
+
+            # create the subscription for the new customer
+            dest_subscription = stripe.Subscription.create(
+                customer=dest_customer.id,
+                items=[{"price": new_price}],
+                trial_end=subscription_end_date,
+                api_key=dest,
             )
 
-        if not dest_customer.email:
-            print("Customer does not have email, will try to update by stripeId")
-            db.users.update(
-                {"stripeId": dest_customer.id},
-                {
-                    "$set": {
-                        "stripeId": dest_customer.id,
-                        "subscriptionId": dest_subscription.id,
-                        "hasSubscription": True,
-                    }
-                },
-                multi=True,
-            )
-        else:
-            db.users.update(
-                {"email": dest_customer.email},
-                {
-                    "$set": {
-                        "stripeId": dest_customer.id,
-                        "subscriptionId": dest_subscription.id,
-                        "hasSubscription": True,
-                    }
-                },
-                multi=True,
-            )
+            print()
+            print("New subscription created:", dest_subscription.id)
+            print("New subscription type:", dest_subscription.plan.nickname)
+            print("New subscription start:", time_str(dest_subscription.start))
+            print()
 
-        print(dest_customer.email, "updated in database")
+            # update the database
+            update_db(dest_customer, dest_subscription)
 
-        # finally, cancel the subscription on the source stripe account
-        old_sub = stripe.Subscription.retrieve(subscription.id, api_key=source)
-        old_sub.delete(at_period_end=True)
+            # finally, cancel the subscription on the source stripe account
+            old_sub = stripe.Subscription.retrieve(subscription.id, api_key=source)
+            old_sub.delete(at_period_end=True)
 
-        print("Old subscription,", subscription.id, "was cancelled. Next!")
-        print()
-        print()
+            print("Old subscription,", subscription.id, "was cancelled. Next!")
+
         old_customers = stripe.Customer.list(
-            limit=100, api_key=source, starting_after=customer.id
+            limit=100, api_key=source, starting_after=src_customer.id
         )
